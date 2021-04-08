4a90e1a91a84108260875757184f6743216698bd
Artur Yaroshinski <62154380+arturyaroshinski@users.noreply.github.com>
2021-04-08 16:29:49 +0300

    feat: complete 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let prev = str[0];
+  let count = 1;
+  let encoded = '';
+
+  for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] === prev) {
+      count++;
+    } else {
+      count = count === 1 ? '' : count;
+      encoded += count + prev;
+      prev = arr[i];
+      count = 1;
+    }
+  }
+
+  return encoded;
 }
 
 module.exports = encodeLine;
